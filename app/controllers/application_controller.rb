f1b1da6c4a1218d948f4c63fb9777d7cfc92ad69
yuliyambg <yuliyambg@gmail.com>
2020-11-01 18:07:47 -0500

    added helper method require_login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,4 +22,10 @@ class ApplicationController < Sinatra::Base
   def current_user
     User.find_by(id: session[:user_id])
   end
+
+  def require_login
+    unless logged_in?
+      redirect '/login'
+    end
+  end
 end
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,17 @@
 class RecipesController < ApplicationController
+  before do
+    require_login
 
+  end
   #CREATE
   get '/recipes/new' do
     # if User.find_by(id: session[:user_id])
-    if logged_in?
-      erb :'recipes/new'
-    else
-      redirect '/login'
-    end
+    # if logged_in?
+    #   erb :'recipes/new'
+    # else
+    #   redirect '/login'
+    #   require_login
+      erb :'/recipes/new'
     end
 
   post '/recipes' do
@@ -28,39 +32,31 @@ class RecipesController < ApplicationController
   #READ
   get '/recipes' do
     # if User.find_by(id: session[:user_id])
-    if logged_in?
+    # require_login
     @recipes = Recipe.all.reverse
     erb :'recipes/index'
-    else
-      redirect '/login'
-      end
   end
 
   #SHOW
   get '/recipes/:id' do
     # if User.find_by(id: session[:user_id])
-      if logged_in?
+    require_login
         @recipe = Recipe.find_by(id: params[:id])
         if @recipe
           erb :'recipes/show'
         else
           redirect '/recipes'
         end
-    else
-      redirect '/login'
-    end
+  end
+
+  #EDIT
+  get '/recipes/:id/edit' do
+      # require_login
+      @recipe = Recipe.find(params[:id])
+      erb :'/recipes/edit'
   end
 
   #UPDATE
-  get '/recipes/:id/edit' do
-    if logged_in?
-      @recipe = Recipe.find(params[:id])
-      erb :'/recipes/edit'
-    else
-      redirect '/login'
-    end
-  end
-
   patch '/recipes/:id' do
     @recipe = Recipe.find(params[:id])
     if !params["recipe"]["title"].empty? &&  !params["recipe"]["preparation"].empty?
