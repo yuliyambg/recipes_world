224d3efda246e3d2db169262aadf09b0623b99c8
yuliyambg <yuliyambg@gmail.com>
2020-11-01 11:07:20 -0500

    add update and delete actions for recipe

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -28,6 +28,28 @@ class RecipesController < ApplicationController
     erb :'recipes/show'
   end
 
+  #UPDATE
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :'/recipes/edit'
+  end
 
+  patch '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    if !params["recipe"]["title"].empty? &&  !params["recipe"]["preparation"].empty?
+      @recipe.update(params["recipe"])
+      redirect "/recipes/#{params[:id]}"
+    else
+       @error = "Data invalid. Please try again."
+       erb :'/recipes/edit'
+      end
+  end
+
+  #DESTROY
+  delete '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect '/recipes'
+    end
 
 end
